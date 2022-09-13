81a0d8093a69aeb8c58e442e9b9e3e3e25958dbc
Tobi John <tobioluwafemi58@gmail.com>
2022-09-13 12:43:11 +0100

    creates env and makes imports

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import os
 import json
 import numpy as np
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 
 
 
 class Scraper:
-    def __init__(self, url):
+    def __init__(self, url: str = 'https://soundcloud.com/discover'):
         self.url = url
         self.driver = webdriver.Chrome()
         self.driver.get(self.url)
